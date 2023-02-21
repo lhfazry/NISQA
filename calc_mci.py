caf1a7150a86fbedeed4bd776f83608cce4283f2
Fazry <lhuqita.fazry@gmail.com>
2023-02-21 15:57:23 +0700

    add new scripts

---
+++ calc_mci.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, h
+
+def main(args):
+    mos = []
+
+    df = pd.read_csv(args.csv)
+    mos_preds = df['mos_pred'].to_numpy()
+    m_mos = mean_confidence_interval(mos_preds)
+
+    print(f"MOS: {m_mcd[0]} \u00b1 {m_mcd[1]}")
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Calculate MOS')
+    parser.add_argument('--csv', help='CSV')
+    
+    main(parser.parse_args())
