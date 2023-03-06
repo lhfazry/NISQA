a82651e1491e80f76e1a495a7272ce3897c9f60c
Fazry <lhuqita.fazry@gmail.com>
2023-03-07 04:16:08 +0700

    add more script

--- run_predict.py
+++ run_predict.py
@@ -47,6 +47,7 @@ if __name__ == "__main__":
     nisqa = nisqaModel(args)
     df = nisqa.predict()
 
+    print(f"\n\n{args.data_dir} - {args.prefix}")
     mos_preds = df['mos_pred'].to_numpy()
     m_mos = mean_confidence_interval(mos_preds)
 
